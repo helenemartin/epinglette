9bd586cd5830095b029e57371ae311b4a24b3f5c
helenemartin <helenemartin13@hotmail.com>
2015-06-24 20:32:06 +0100

    affix image to each catogory add image to asset folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create({name: "Art", image: "red"})
+Category.create({name: "Film", image: "ming"})
+Category.create({name: "Sculpture", image: "black"})
+Category.create({name: "Sewing", image: "orange"})
+Category.create({name: "Photography", image: "purple"})
+Category.create({name: "Video", image: "cream"})
