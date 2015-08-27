5944062a05b34970b2e74c492b4a9e3aeffd1387
helenemartin <helenemartin13@hotmail.com>
2015-08-27 02:44:58 +0100

    add website url to user model, update form in new user session and edit user session

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804182616) do
+ActiveRecord::Schema.define(version: 20150826194930) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20150804182616) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "website_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
