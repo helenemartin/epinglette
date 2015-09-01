ffc79915b10f6efe7120aee0d438aedc6b23af21
helenemartin <helenemartin13@hotmail.com>
2015-09-01 20:06:05 +0100

    add chronological orders on pins for user show page

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,12 +1,26 @@
 class MembersController < ApplicationController
   def show
-  
     @user= User.find(params[:id])
-    @pins= @user.pins
+    @pins= @user.pins.order("created_at DESC")
+
+   
 
   end
   def index
     @users= User.all
+  end
+
+  def update
+    if @user.update(pin_params)
+      redirect_to @user, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+# Never trust parameters from the scary internet, only allow the white list through.
+  def member_params
+    params.require(:pin).permit(:password, :website_url, :name)
   end
 
 end
