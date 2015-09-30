5a54d96b5b029673164c4bd11b5dbca1d7caaca1
helenemartin <helenemartin13@hotmail.com>
2015-09-30 11:56:59 +0100

    change image resolution for user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   has_many :pins, dependent: :destroy
   
   has_attached_file :avatar, 
-    styles: { medium: "300x300>", thumb: ""},
+    styles: { medium: "200X200", thumb: ""},
       convert_options: { thumb: "-gravity Center -crop 500x500+0+0 +repage -resize 100x100^"}, default_url:"/images/:style/missing.png"
 
 
