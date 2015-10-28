381273d0b6b4ddb1640402821624669e6e7a7982
helenemartin <helenemartin13@hotmail.com>
2015-10-28 20:53:52 +0000

    Try again to fix default image on user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, 
     styles: { medium: "200X200#", thumb: ""},
     convert_options: { thumb: "-gravity Center -crop 500x500+0+0 +repage -resize 100x100^"},
-    default_url: ("/assets/ming.png")
+    default_url: ActionController::Base.helpers.asset_path("/assets/ming.png")
 
 
   validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/ , /gif\Z/]
