a9b8818e246c63b9ce9e722aeda9e3089b89888e
helenemartin <helenemartin13@hotmail.com>
2015-10-28 19:40:38 +0000

    Use asset helper in User.rb for default image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -15,7 +16,8 @@ class User < ActiveRecord::Base
   
   has_attached_file :avatar, 
     styles: { medium: "200X200#", thumb: ""},
-      convert_options: { thumb: "-gravity Center -crop 500x500+0+0 +repage -resize 100x100^"}, default_url:"/assets/ming.png"
+    convert_options: { thumb: "-gravity Center -crop 500x500+0+0 +repage -resize 100x100^"},
+    default_url: ActionController::Base.helpers.asset_path("/assets/ming.png")
 
 
   validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/ , /gif\Z/]
