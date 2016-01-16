69f2afaeda79997878252485d732545442a58c8f
helenemartin <helenemartin13@hotmail.com>
2016-01-16 12:47:54 +0000

    add s3 protocol for https urls

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
     belongs_to :user
     belongs_to :category
-    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100" }
+    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100" }, :s3_protocol => :https
     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
     validates :image, presence: true, unless: ->(pin){pin.video_url.present?}
     validates :video_url, presence: true, unless: ->(pin){pin.image.present?}
